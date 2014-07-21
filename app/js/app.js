caad4739ed80b60fa65ffe226b4dbf344c11064a
Cristhian Duran <durancristhian@gmail.com>
2014-07-21 01:16:45 -0300

    Firsts steps with angular

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,48 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ["ngStorage"]);
 
-myApp.controller("myAppController", function ($scope) {
+myApp.controller("myAppController", function ($scope, $localStorage) {
 
 	$scope.init = function () {
 
-		$scope.greeting = "Hello user! (:";
-	};
+		$scope.storage = $localStorage.$reset({
+			todoLists: [{
+				name: "My list",
+				tasks: []
+			}]
+		});
+
+		$scope.showTasks = false;
+		$scope.newTask = "";
+	}
+
+	$scope.getAmountOfDoneTasks = function (todoList) {
+
+		var amount = 0;
+
+		angular.forEach(todoList.tasks, function (task) {
+
+			if(task.done)
+				amount++;
+		});
+
+		return amount;
+	}
+
+	$scope.showInformation = function () {
+
+		alert("Be patient. I'm working on this functionality.");
+	}
+
+	$scope.addTask = function (todoList) {
+
+		if($scope.newTask !== undefined && $scope.newTask !== "") {
+
+			todoList.tasks.push({
+				text: $scope.newTask,
+				done: false
+			});
+
+			$scope.newTask = "";
+		}
+	}
 });
