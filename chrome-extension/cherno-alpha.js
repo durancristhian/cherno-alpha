5d4daa513e40b36c2ff7886d21e4aa2e737b1134
Cristhian Duran <durancristhian@gmail.com>
2014-08-05 21:27:35 -0300

    Added maximized state when you open the app

--- chrome-extension/cherno-alpha.js
+++ chrome-extension/cherno-alpha.js
@@ -1,5 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('chrome-extension/home.html', {
+		"state": "maximized",
 		"bounds": {
 			"width": window.screen.availWidth,
 			"height": window.screen.availHeight
